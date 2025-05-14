81746151fb076df3a183456da2d33634b310288b
fesabelilla <fesabelilla@gmail.com>
2025-05-14 16:28:24 +0600

    Basic Router Impl Done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Favorites from "./pages/Favorites";
 import Home from "./pages/Home";
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <main className="main-content">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Routes>
+    </main>
   );
 }
 
